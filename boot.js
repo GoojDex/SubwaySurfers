2b5276eba0eae81ab1107456d2ed25ed71307d2f
GoojDex <114043672+GoojDex@users.noreply.github.com>
2023-03-18 13:41:14 +0530

    Update boot.js

--- boot.js
+++ boot.js
@@ -74,7 +74,7 @@
 
       function initApp(e) {
           var n, t, a;
-          n = e.meta, t = pathJoin(n.path, n.splash), (a = document.querySelector("#og-game-holder")).style.backgroundColor = n.color, a.style.backgroundImage = 'url("' + t + '?v=1gefx1jwdk0gj12hb"")', loadFont("assets/font/lilita-one.css", "Lilita One"), loadFont("assets/font/titan-one.css", "Titan One"), window.sharedAppData.config.leaderboards && loadScript("js/leaderboard.js"), window.sharedAppData.config.vconsole && loadScript("https://cdnjs.cloudflare.com/ajax/libs/vConsole/3.3.0/vconsole.min.js"), loadScript("js/inflate.min.js"), loadScript("js/vendor.js"), loadScript("js/main.js")
+          n = e.meta, t = pathJoin(n.path, n.splash), (a = document.querySelector("#og-game-holder")).style.backgroundColor = n.color, a.style.backgroundImage = 'url("' + t + '?v=1gefx1jwdk0gj12hb"")', loadFont("/lilita-one.css", "Lilita One"), loadFont("/titan-one.css", "Titan One"), window.sharedAppData.config.leaderboards && loadScript("/leaderboard.js"), window.sharedAppData.config.vconsole && loadScript("https://cdnjs.cloudflare.com/ajax/libs/vConsole/3.3.0/vconsole.min.js"), loadScript("/inflate.min.js"), loadScript("/vendor.js"), loadScript("/main.js")
       }
       console.log = function() {
           return null
@@ -102,7 +102,7 @@
                   meta: {
                       path: "./",
                       color: "#ffa4cd",
-                      splash: "/assets/preload/splash_mip.png"
+                      splash: "/splash_mip.png"
                   }
               }
           }, e = t.pokiSdkDebug, n = function(e) {
